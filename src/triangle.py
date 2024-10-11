3dbf845e042ded0b324461c481c7766bdfeca4bd
v.shalginov <slava8427@mail.ru>
2024-10-11 19:52:47 +0700

    added triangle existing condition

--- src/triangle.py
+++ src/triangle.py
@@ -6,6 +6,12 @@ class Triangle(Figure):
     def __init__(self, side_a, side_b, side_c) -> None:
         if side_a <= 0 or side_b <= 0 or side_c <= 0:
             raise ValueError("Triangle sides can`t be less than 0")
+        if (
+            side_a + side_b <= side_c
+            or side_a + side_c <= side_b
+            or side_b + side_c <= side_a
+        ):
+            raise ValueError("The triangle does not exist")
         self.side_a = side_a
         self.side_b = side_b
         self.side_c = side_c
